1d1738e53490048ee222a57e67ba56de2c354cef
Anastasiia Barabash <nastya.barabash@gmail.com>
2025-11-25 17:16:04 +0200

    adding FileSystem to store data

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Request, Response, Router } from "express";
+import fs from "fs";
+import path from "path";
 
 const router: Router = Router();
 
@@ -7,7 +9,35 @@ type TUser = {
   todos: string[];
 };
 
+const dataFile = path.join(process.cwd(), "data.json");
 let users: TUser[] = [];
+
+fs.readFile(dataFile, "utf8", (err, data) => {
+  if (err) {
+    if (err.code === "ENOENT") {
+      fs.writeFile(dataFile, JSON.stringify([]), (err) => {
+        if (err) console.error("Error creating data.json:", err);
+      });
+    } else {
+      console.error("Error reading data.json:", err);
+    }
+    users = [];
+    return;
+  }
+
+  try {
+    users = JSON.parse(data) as TUser[];
+  } catch (error: any) {
+    console.error(`Error parsing JSON: ${error}`);
+    users = [];
+  }
+});
+
+function saveUsers() {
+  fs.writeFile(dataFile, JSON.stringify(users, null, 2), (err) => {
+    if (err) console.error("Error writing data.json:", err);
+  });
+}
 
 router.get("/users", (req: Request, res: Response) => {
   res.json(users);
@@ -93,7 +123,7 @@ router.put("/update", (req: Request, res: Response) => {
   }
 
   user.todos.splice(todoIndex, 1);
-
+  saveUsers();
   res.json({ message: "Todo deleted successfully." });
 });
 
