0e55e3368a465b2b782df6bbbbf60e10dbb59cb0
Anastasiia Barabash <nastya.barabash@gmail.com>
2025-12-05 14:34:46 +0200

    copying previous week code

---
+++ week6/data/users.ts
@@ -0,0 +1,14 @@
+type User = {
+  name: string
+}
+
+const populateUsers: User[] = [
+  {"name": "Jukka"},
+  {"name": "Jukka"},
+  {"name": "Jukka"},
+  {"name": "Jukka"},
+  {"name": "Jukka"},
+  {"name": "Jukka"}
+]
+
+export default populateUsers;
