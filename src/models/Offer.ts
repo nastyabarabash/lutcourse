3ddea1e377a07381adaaa7f7195673b67562a3c6
Anastasiia Barabash <nastya.barabash@gmail.com>
2025-12-08 23:35:17 +0200

    editing html

--- src/models/Offer.ts
+++ src/models/Offer.ts
@@ -4,12 +4,14 @@ interface IOffer extends Document {
   title: string
   description: string
   price: number
+  imageId?: string
 }
 
 const offerSchema: Schema<IOffer> = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  price: { type: Number, required: true }
+  price: { type: Number, required: true },
+  imageId: { type: String }
 });
 
 const Offer = mongoose.model<IOffer>("Offer", offerSchema);
