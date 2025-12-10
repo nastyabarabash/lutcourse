0efce71b02344cad7385081583b89f2dc1998310
Anastasiia Barabash <nastya.barabash@gmail.com>
2025-12-10 16:40:12 +0200

    editing js file

--- public/js/offer.js
+++ public/js/offer.js
@@ -1,24 +1,6 @@
 const form = document.getElementById("offerForm");
 const offersContainer = document.getElementById("offersContainer");
 
-// function renderOffers(offers) {
-//   offersContainer.innerHTML = "";
-
-//   offers.forEach(offer => {
-//     const div = document.createElement("div");
-//     div.className = "offerDiv col s12 m6 l4";
-
-//     div.innerHTML = `
-//       <img src="${offer.imagePath || '/images/puzzle.jpg'}" class="responsive-img" />
-//       <p><strong>${offer.title}</strong></p>
-//       <p>${offer.description}</p>
-//       <p>Price: €${offer.price}</p>
-//     `;
-
-//     offersContainer.appendChild(div);
-//   });
-// }
-
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
@@ -33,7 +15,6 @@ form.addEventListener("submit", async (e) => {
     const data = await res.json();
     if (res.ok) {
       form.reset();
-      loadOffers();
     } else {
       console.error("Error submitting offer:", data.message);
     }
