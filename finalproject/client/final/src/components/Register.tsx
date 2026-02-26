dcad9ddee42a921416216f6d3c2b52b2a6e7057e
Anastasiia Barabash <nastya.barabash@gmail.com>
2026-02-26 10:53:13 +0200

    functioning login and registration

--- finalproject/client/final/src/components/Register.tsx
+++ finalproject/client/final/src/components/Register.tsx
@@ -1,19 +1,19 @@
 import { useState } from "react"
 
 const Register = () => {
-  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
 
     try {
-      const response = await fetch("http://localhost:3000/api/auth/register", {
+      const response = await fetch("http://localhost:5000/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({ email, password }),
       })
 
       const data = await response.json()
@@ -34,9 +34,9 @@ const Register = () => {
       <h2>Register</h2>
 
       <input
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
 
       <input
